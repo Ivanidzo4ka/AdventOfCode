345502e6e95ae5164dde3c48e14f57be3534140f
Ivanidze <ivmatan@microsoft.com>
2024-11-21 22:46:16 -0800

    2016 day 1,2,3

---
+++ 2016/Day02/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+PartOne();
+PartTwo();
+void PartOne()
+{
+
+    var x = 1;
+    var y = 1;
+
+    var lines = File.ReadAllLines("input.txt");
+    var pad = new char[3][]{
+    new char[]{'1', '2', '3'},
+    new char[]{'4', '5', '6'},
+    new char[]{'7', '8', '9'}};
+    var ans = new StringBuilder();
+    foreach (var line in lines)
+    {
+        foreach (var move in line)
+        {
+            switch (move)
+            {
+                case 'U': if (x > 0) x--; break;
+                case 'D': if (x < 2) x++; break;
+                case 'R': if (y < 2) y++; break;
+                case 'L': if (y > 0) y--; break;
+            }
+        }
+        ans.Append(pad[x][y]);
+    }
+    Console.WriteLine(ans.ToString());
+}
+
+void PartTwo()
+{
+
+    var x = 3;
+    var y = 1;
+
+    var lines = File.ReadAllLines("input.txt");
+    var pad = new string[7]{
+        "       ",
+        "   1   ",
+        "  234  ",
+        " 56789 ",
+        "  ABC  ",
+        "   D   ",
+        "       "
+    };
+ 
+    var ans = new StringBuilder();
+    foreach (var line in lines)
+    {
+        foreach (var move in line)
+        {
+            var newx=x;
+            var newy=y;
+            switch (move)
+            {
+                case 'U': newx--; break;
+                case 'D': newx++; break;
+                case 'R': newy++; break;
+                case 'L': newy--; break;
+            }
+            if (pad[newx][newy]!=' ')
+            {
+                x = newx;
+                y = newy;
+            }
+        }
+        ans.Append(pad[x][y]);
+    }
+    Console.WriteLine(ans.ToString());
+}
---
+++ 2016/Day03/Program.cs
@@ -0,0 +1,47 @@
+﻿PartOne();
+PartTwo();
+void PartOne(){
+      var lines = File.ReadAllLines("input.txt");
+      var ans=0;
+      foreach(var line in lines)
+      {
+        var split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray();
+        if (IsTriangle(split))
+            ans++;
+      }
+      Console.WriteLine(ans);
+}
+bool IsTriangle(IList<int> points)
+{
+       if (points[0]+points[1]<=points[2]){  return false;}
+        if (points[0]+points[2]<=points[1]){  return false;}
+        if (points[1]+points[2]<=points[0]){  return false;}
+        return true;
+}
+void PartTwo(){
+      var lines = File.ReadAllLines("input.txt");
+      var ans=0;
+      var a = new List<int>();
+      var b =new List<int>();
+      var c  = new List<int>();
+      foreach(var line in lines)
+      {
+        var split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray();
+        a.Add(split[0]);
+        b.Add(split[1]);
+        c.Add(split[2]);
+        if (a.Count==3)
+        {
+            if (IsTriangle(a))
+                ans++;
+            if (IsTriangle(b))
+                ans++;
+            if (IsTriangle(c))
+                ans++;
+            a.Clear();
+            b.Clear();
+            c.Clear();
+        }
+      }
+      Console.WriteLine(ans);
+}
