8449e99fe6aa02a5cbdd141007364e49266814bc
Ivanidze <ivmatan@microsoft.com>
2024-11-27 17:06:12 -0800

    Day6

---
+++ 2016/Day06/Program.cs
@@ -0,0 +1,33 @@
+﻿PartOne();
+PartTwo();
+void PartOne()
+{
+     var lines = File.ReadAllLines("input.txt");
+     for(int i=0; i< lines[0].Length; i++)
+     {
+        var freq = new Dictionary<char, int>();
+        foreach(var line in lines)
+        {
+            freq.TryGetValue(line[i], out var c);
+            freq[line[i]] = c+1;
+        }
+        Console.Write(freq.OrderByDescending(x=>x.Value).First().Key);
+     }
+     Console.WriteLine();
+}
+
+void PartTwo()
+{
+     var lines = File.ReadAllLines("input.txt");
+     for(int i=0; i< lines[0].Length; i++)
+     {
+        var freq = new Dictionary<char, int>();
+        foreach(var line in lines)
+        {
+            freq.TryGetValue(line[i], out var c);
+            freq[line[i]] = c+1;
+        }
+        Console.Write(freq.OrderBy(x=>x.Value).First().Key);
+     }
+     Console.WriteLine();
+}
