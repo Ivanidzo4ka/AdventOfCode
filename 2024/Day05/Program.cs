9b76daf0b8f181c4ff60a3bdc48908200134ca28
Ivanidze <ivmatan@microsoft.com>
2024-12-05 21:52:21 -0800

    Day05 Day06

---
+++ 2024/Day05/Program.cs
@@ -0,0 +1,103 @@
+﻿PartOne(true);
+PartOne(false);
+PartTwo(true);
+PartTwo(false);
+void PartOne(bool test = false)
+{
+    var data = ReadData(test);
+
+    var ans = 0L;
+    if (test)
+        Console.Write("Test:");
+    foreach (var page in data.pages)
+    {
+        if (IsGood(page, data.rules))
+            ans += page[page.Count / 2];
+    }
+    Console.WriteLine(ans);
+}
+bool IsGood(List<int> page, List<int[]> rules)
+{
+    var set = new HashSet<int>();
+    for (int i = 0; i < page.Count; i++)
+    {
+        foreach (var rule in rules)
+        {
+            if (rule[0] == page[i])
+            {
+                if (set.Contains(rule[1])) return false;
+            }
+        }
+        set.Add(page[i]);
+    }
+    return true;
+
+}
+void PartTwo(bool test = false)
+{
+    var data = ReadData(test);
+    var ans = 0L;
+    if (test)
+        Console.Write("Test:");
+    foreach (var page in data.pages)
+    {
+        if (!IsGood(page, data.rules))
+        {
+            Reorder(page, data.rules);
+            ans += page[page.Count / 2];
+        }
+    }
+    Console.WriteLine(ans);
+}
+void Reorder(List<int> page, List<int[]> rules)
+{
+    var newOne = new List<int>();
+    var set = page.ToHashSet();
+    while (newOne.Count != page.Count)
+    {
+        var counts = new Dictionary<int, int>();
+        foreach (var elem in set)
+        {
+            foreach (var rule in rules)
+            {
+                if (rule[1] == elem && set.Contains(rule[0]))
+                {
+                    counts.TryGetValue(elem, out var count);
+                    counts[elem] = count + 1;
+                }
+            }
+        }
+        foreach (var elem in set)
+        {
+            if (!counts.ContainsKey(elem))
+            {
+                newOne.Add(elem);
+                set.Remove(elem);
+            }
+        }
+    }
+    page.Clear();
+    foreach (var elem in newOne)
+        page.Add(elem);
+
+}
+
+(List<int[]> rules, List<List<int>> pages) ReadData(bool test = false)
+{
+    var lines = File.ReadAllLines(test ? "test.txt" : "input.txt");
+    var pos = 0;
+    var rules = new List<int[]>();
+    var pages = new List<List<int>>();
+    while (lines[pos] != "")
+    {
+        rules.Add(lines[pos].Split("|").Select(x => int.Parse(x)).ToArray());
+        pos++;
+    }
+    pos++;
+    while (pos < lines.Length)
+    {
+        pages.Add(lines[pos].Split(",").Select(x => int.Parse(x)).ToList());
+        pos++;
+    }
+    return (rules, pages);
+}
---
+++ 2024/Day06/Program.cs
@@ -0,0 +1,98 @@
+﻿PartOne(true);
+PartOne(false);
+PartTwo(true);
+PartTwo(false);
+void PartOne(bool test = false)
+{
+    var data = ReadData(test);
+    var n = data.Length;
+    var dir = new (int x, int y)[] { (-1, 0), (0, 1), (1, 0), (0, -1) };
+    var curD = 0;
+    var ans = 0L;
+    var startx = 0;
+    var starty = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (data[i][j] == '^')
+            {
+                startx = i;
+                starty = j;
+                break;
+            }
+        }
+    if (test)
+        Console.Write("Test:");
+    var set = new HashSet<(int x, int y)>();
+    do
+    {
+        set.Add((startx, starty));
+        var nextx = startx + dir[curD].x;
+        var nexty = starty + dir[curD].y;
+
+        if (nextx < 0 || nextx == n || nexty < 0 || nexty == n) break;
+        if (data[nextx][nexty] == '#') { curD++; curD %= 4; }
+        else { startx = nextx; starty = nexty; }
+    } while (true);
+
+    Console.WriteLine(set.Count);
+}
+void PartTwo(bool test = false)
+{
+    var data = ReadData(test);
+    var ans = 0L;
+    var n = data.Length;
+    var startx = 0;
+    var starty = 0;
+    if (test)
+        Console.Write("Test:");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (data[i][j] == '^')
+            {
+                startx = i;
+                starty = j;
+                break;
+            }
+        }
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (data[i][j] == '.')
+            {
+                data[i][j] = '#';
+                if (IsLoop(data, startx, starty))
+                {
+                    ans++;
+                }
+                data[i][j] = '.';
+            }
+        }
+    Console.WriteLine(ans);
+}
+
+bool IsLoop(char[][] data, int startx, int starty)
+{
+    var n = data.Length;
+    var dir = new (int x, int y)[] { (-1, 0), (0, 1), (1, 0), (0, -1) };
+    var curD = 0;
+    var set = new HashSet<(int x, int y, int d)>();
+    do
+    {
+        if (!set.Add((startx, starty, curD)))
+        { return true; }
+        var nextx = startx + dir[curD].x;
+        var nexty = starty + dir[curD].y;
+
+        if (nextx < 0 || nextx == n || nexty < 0 || nexty == n) return false;;
+        if (data[nextx][nexty] == '#') { curD++; curD %= 4; }
+        else { startx = nextx; starty = nexty; }
+    } while (true);
+}
+
+char[][] ReadData(bool test = false)
+{
+    var lines = File.ReadAllLines(test ? "test.txt" : "input.txt");
+    return lines.Select(x => x.ToCharArray()).ToArray();
+}
